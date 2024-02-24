805d5be2e5f77d83a06aaa8da1a9bda90898b7de
goodnessaig1 <goodness6337@gmail.com>
2024-02-24 20:08:43 +0100

    Refractoring home page

--- src/components/HomePage/HeroSection.jsx
+++ src/components/HomePage/HeroSection.jsx
@@ -12,6 +12,7 @@ import Cursor from '../../assets/cursor.svg';
 import Marquee from 'react-fast-marquee';
 import Sideber from '../HOC/Sideber';
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const HeroSection = () => {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -33,11 +34,48 @@ const HeroSection = () => {
                 <img src={Logo} className='sm:hidden lg:flex' alt='' />
                 <img src={Logo2} className='sm:flex lg:hidden' alt='' />
               </div>
-              <div className='sm:hidden lg:flex flex-row lg:gap-[36px] xxl:gap-[64px] '>
-                <span className='z-10 hover:cursor-pointer'>HOME</span>
-                <span className='z-10 hover:cursor-pointer'>COURSES</span>
-                <span className='z-10 hover:cursor-pointer'>BLOG</span>
-                <span className='z-10 hover:cursor-pointer'>ABOUT US</span>
+              <div className='sm:hidden z-10 lg:flex flex-row lg:gap-[36px] xxl:gap-[64px] '>
+                <NavLink
+                  exact
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'nav-link'
+                      : 'unselected hover:text-[#000] transition duration-200'
+                  }
+                  to='/'
+                >
+                  <h1>HOME</h1>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'nav-link'
+                      : 'unselected hover:text-[#000] transition duration-200'
+                  }
+                  to='/courses'
+                >
+                  <h1>COURSES</h1>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'nav-link'
+                      : 'unselected hover:text-[#000] transition duration-200'
+                  }
+                  to='/blog'
+                >
+                  <h1>BLOG</h1>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'nav-link'
+                      : 'unselected hover:text-[#000] transition duration-200'
+                  }
+                  to='/about-us'
+                >
+                  <h1>ABOUT US</h1>
+                </NavLink>
               </div>
               <div className='sm:hidden lg:flex z-10 bg-[#fff] hover:bg-[#f0f0f0] rounded-[16px] hover:cursor-pointer border border-[1px] border-[#1c1c1c] p-[10px] '>
                 <span>Payment Guide</span>
